cb9b405562aa091dff310fd82a548ab5f41d1e9d
Dwscdv3 <dwscdv3@hotmail.com>
2022-12-21 10:59:50 +0800

    fix: do not migrate on fresh new databases

--- src/migration/index.ts
+++ src/migration/index.ts
@@ -4,7 +4,7 @@ import migration_2 from './2.js';
 
 export const migrations: Migration[] = [
     migration_1,
-    migration_2
+    migration_2,
 ].sort((a, b) => a.version - b.version);
 
 export async function migrate() {
@@ -12,12 +12,13 @@ export async function migrate() {
     try {
         version = parseFloat(await db.get('version'));
     } catch {
-        version = 0;
+        version = migrations[migrations.length - 1].version;
+        await db.put('version', version.toString());
     }
     console.log('Current database version: ' + version);
     for (const migration of migrations) {
         if (migration.version > version) {
-            console.log('Migrating database to version ' + version + '...');
+            console.log('Migrating database to version ' + migration.version + '...');
             // Should crash if error has occured
             await migration.up();
             version = migration.version;
